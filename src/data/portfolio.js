bb888be61efc0657889ae94426cb28ab842e7638
haki <haki@N-EX4231>
2022-12-09 12:03:05 +0100

    dd

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default [
     {
         title: 'Game',
-        imgUrl: "/src/assets/Game.jpg",
+        imgUrl: "/assets/Game.jpg",
         stack: ['Python'],
         link: 'https://github.com/hadiki/Game'
     },
     {
         title: 'Traffic Light',
-        imgUrl: '/src/assets/TrafficLight.jpg',
+        imgUrl: '/assets/TrafficLight.jpg',
         stack: ['.NET', 'C#'],
         link: 'https://github.com/hadiki/TrafficlLight'
     },
     {
         title: 'Minimal Portfolio',
-        imgUrl: '/src/assets/Portfolio.jpg',
+        imgUrl: '/assets/Portfolio.jpg',
         stack: ['Javascript', 'React', 'Vite'],
         link: 'https://github.com/hadiki/Minimal-Portfolio'
     }
