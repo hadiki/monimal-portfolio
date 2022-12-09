3bf1c7e5a8b63f4b9730baca615ebaaedfa4b8ce
haki <haki@N-EX4231>
2022-12-09 11:53:26 +0100

    banana

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,7 +46,7 @@ function Footer() {
 					</div>
 				</div>
             <p className="text-sm mt-2 opacity-50">
-                &copy; {new Date().getFullYear()} Hadi Kiwan. All rights reserved.
+                &copy; {new Date().getFullYear()} Hadi Kiwan. All rights reserved. test
             </p>
         </div>
     )
